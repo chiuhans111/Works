aed72e9874f8084a7f82b4126e2c77f3789b777c
chiuhans111 <chiuhans111@gmail.com>
2016-04-03 23:22:56 +0900

    meta omg

--- CodingLesson/superToolDev.js
+++ CodingLesson/superToolDev.js
@@ -84,6 +84,7 @@ sptool.auto.all = function(){
   sptool.setup([sptool.auto.jquery,sptool.auto.bootstrap]);
 }
 sptool.auto.forClass = function(title){
+  sptool.doc.appendToHead(sptool.doc.create("meta",{charset:"UTF-8"}));
   sptool.auto.all();
   var title = sptool.doc.create("title")
   title.textContent = "乙班09邱柏翰"+title;
@@ -96,8 +97,7 @@ sptool.auto.jquery = function(){
   sptool.doc.import.js("https://code.jquery.com/jquery-migrate-1.2.1.min.js",
                        "jqueryMin","jquery");
 }
-sptool.auto.bootstrap = function(){  
-  sptool.doc.appendToHead(sptool.doc.create("meta",{charset:"UTF-8"}));
+sptool.auto.bootstrap = function(){
   sptool.doc.appendToHead(sptool.doc.create("meta",{
     name:"viewport", content:"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
   }));
