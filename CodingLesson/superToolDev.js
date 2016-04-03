874661c13472585b5963d34224bf5c193d251640
chiuhans111 <chiuhans111@gmail.com>
2016-04-04 01:23:08 +0900

    small

--- CodingLesson/superToolDev.js
+++ CodingLesson/superToolDev.js
@@ -169,8 +169,10 @@ sptool.doc.setOnBootstrapMode = function(){
   newContain.appendChild(row1);
   newContain.appendChild(row2);
   row1.innerHTML = oldText;
-  row2.innerHTML = "<hr><br>此為我學過bootstrap以及javascript後<br>"+
-    "寫了個小程式幫我自動套用bootstrap版型 by 0304207";
+  row2.innerHTML = "<hr><br>"+
+    "為了拯救老師的眼睛，不需要再看到擠在左側的網頁"+
+    "所以在我學過bootstrap以及javascript之後<br>"+
+    "寫了個小程式幫我自動套用bootstrap版型<br>by 0304207";
   sptool.doc.body.appendChild(newContain);
   
   var table = newContain.getElementsByTagName("table");
